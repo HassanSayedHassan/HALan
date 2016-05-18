9378f6359d73e2b4def5e72caebf7a85a5b6f2da
signorpiero <signorpiero@gmail.com>
2016-05-18 15:47:26 +0200

    prova

--- app/src/main/java/com/example/signo/halan/PrimoAvvio.java
+++ app/src/main/java/com/example/signo/halan/PrimoAvvio.java
@@ -2,15 +2,12 @@ package com.example.signo.halan;
 
 import android.util.Log;
 
-/**
- * Created by signo on 07/05/2016.
- */
+
 public class PrimoAvvio {
 
     public void aggiungiLivelli(){
 
-        Log.w("primo","avvio");
-
+        //ciao
 
     }
 
