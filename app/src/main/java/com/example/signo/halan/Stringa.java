a5fd5fa5a1fe8647a11da4ee168b53ba94d32916
signorpiero <signorpiero@gmail.com>
2016-06-08 12:05:16 +0200

    Qualche piccola modifica

--- app/src/main/java/com/example/signo/halan/Stringa.java
+++ app/src/main/java/com/example/signo/halan/Stringa.java
@@ -79,70 +79,7 @@ public class Stringa {
     }
 
     public DescrittoreLivelloServer splitRisposta(){
-        /*
-        int numerolivelli;
-        int count=0, j=0, k=1;
-        char[] temp = new char[100];
-        String chr, str;
 
-        risposta = new DescrittoreLivelloServer();
-        this.stringa = stringa;
-
-        char[] stringaSplitted = stringa.toCharArray();
-        Log.w("stringa", String.valueOf(stringaSplitted[0]));
-        numerolivelli = Integer.parseInt(String.valueOf(stringaSplitted[0]));
-
-        risposta.setLivelli(numerolivelli);
-        //ciclo per le righe del database count=riga corrente numerolivelli=righe tot
-        for (count = 0; count< numerolivelli; count++) {
-            //k variabile di stato K=1 autore, k=2 esposto...
-
-            //ciclo per scorrere la stringa
-            for (int i = 1; i < stringa.length(); i++) {
-
-                chr = String.valueOf(stringaSplitted[i]);
-                Log.w("stringa",chr);
-                //controllo caratteri
-                if (!chr.equals("@")) {
-                    //stringa di appoggio contiene momentaneamente la parola
-                    temp[j] = stringaSplitted[i];
-                    j++;
-                }
-                    else {
-                    //parola terminata la devo scrivere in descrittorelivello
-                    j = 0;
-
-                    str = String.valueOf(temp);
-
-                    Log.w("temp", str);
-                    switch (k) {
-                        case 1:
-                            risposta.setAutore(str, count);
-                            break;
-                        case 2:
-                            risposta.setEsposto(str, count);
-                            break;
-                        case 3:
-                            risposta.setSoluzione(str, count);
-                            break;
-                        case 4:
-                            risposta.setSoluzione2(str, count);
-                            break;
-                    }
-                    //la prossima parola sarà esposto
-                    k++;
-                }
-
-
-            }
-            // dopo che k=4 devo ricominciare
-            count++;
-            k=1;
-        }
-
-
-        return risposta;
-        */
 
         int livelliDaAggiungere;
         //splitto la string con tappo @
